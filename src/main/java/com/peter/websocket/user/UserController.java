7d50ff2916e6fab4c2381b0883f55b7461ad7653
badfff2 <shzbjh@gmail.com>
2026-08-22 17:46:07 +0800

    fix: remove unused import in UserController

--- src/main/java/com/peter/websocket/user/UserController.java
+++ src/main/java/com/peter/websocket/user/UserController.java
@@ -1,6 +1,5 @@
 package com.peter.websocket.user;
 
-import com.peter.websocket.chat.ChatMessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
